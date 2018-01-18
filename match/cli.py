07d446a35f921fe6de87698bb9d5867b7272dcb8
Steven Brien <sbrien@hlkagency.com>
2018-01-18 12:27:26 -0600

    Fix cli output

--- match/cli.py
+++ match/cli.py
@@ -8,9 +8,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for match."""
-    click.echo("Replace this message by putting your code into "
-               "match.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo("Nothing to see here")
 
 
 if __name__ == "__main__":
